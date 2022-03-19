488aa14c67a67a4357f5741b12f2a082f7a4e5a6
Glauber Silva <info@glaubersilva.me>
2022-03-19 17:03:35 -0300

    Add "test_comment_get" for the API V1

--- tests/test-wapuus-api-v1-comment-tests.php
+++ tests/test-wapuus-api-v1-comment-tests.php
@@ -4,6 +4,9 @@ namespace Wapuus_API\Tests;
 
 class Wapuus_API_V1_Comment_Tests extends Unit_API_Test_Case {
 
+	public $comment_sample = 'This is a sample comment! =)';
+	public $post_sample_id;
+
 	/**
 	 * A cada teste o WordPress é zerado e volta ao seu estado de logo após a instalação.
 	 *
@@ -15,6 +18,14 @@ class Wapuus_API_V1_Comment_Tests extends Unit_API_Test_Case {
 	 */
 	public function set_up() {
 		parent::set_up();
+
+		$this->post_sample_id = $this->factory->post->create(
+			array(
+				'post_title'  => 'My Wapuu',
+				'post_type'   => 'wapuu',
+				'post_status' => 'publish',
+			)
+		);
 	}
 
 	public function tear_down() {
@@ -23,20 +34,10 @@ class Wapuus_API_V1_Comment_Tests extends Unit_API_Test_Case {
 
 	public function test_comment_post() {
 
-		$post_id = $this->factory->post->create(
-			array(
-				'post_title'  => 'My Wapuu',
-				'post_type'   => 'wapuu',
-				'post_status' => 'publish',
-			)
-		);
-
-		$request = new \WP_REST_Request( 'POST', '/wapuus-api/v1/comment/' . $post_id );
-
-		$comment = 'Beautiful colors! =)';
+		$request = new \WP_REST_Request( 'POST', '/wapuus-api/v1/comment/' . $this->post_sample_id );
 
 		$request_query = array(
-			'comment' => $comment,
+			'comment' => $this->comment_sample,
 		);
 
 		$request->set_query_params( $request_query );
@@ -48,8 +49,44 @@ class Wapuus_API_V1_Comment_Tests extends Unit_API_Test_Case {
 		$this->assertEquals( $expected, $result );
 
 		$data     = $response->get_data();
-		$expected = $comment;
+		$expected = $this->comment_sample;
 		$result   = $data->comment_content;
+		$this->assertEquals( $expected, $result );
+
+		$headers  = $response->get_headers();
+		$expected = $headers;
+		$result   = array();
+		$this->assertEquals( $expected, $result );
+	}
+
+	public function test_comment_get() {
+
+		$user = wp_get_current_user();
+
+		$response = array(
+			'user_id'         => $user->ID,
+			'comment_author'  => $user->user_login,
+			'comment_content' => $this->comment_sample,
+			'comment_post_ID' => $this->post_sample_id,
+		);
+
+		$comment_id = wp_insert_comment( $response );
+
+		$request = new \WP_REST_Request( 'GET', '/wapuus-api/v1/comment/' . $this->post_sample_id );
+
+		$response = $this->server->dispatch( $request );
+
+		$expected = 200;
+		$result   = $response->get_status();
+		$this->assertEquals( $expected, $result );
+
+		$data     = $response->get_data();
+		$expected = $this->comment_sample;
+		$result   = $data[0]->comment_content;
+		$this->assertEquals( $expected, $result );
+
+		$expected = $comment_id;
+		$result   = $data[0]->comment_ID;
 		$this->assertEquals( $expected, $result );
 
 		$headers  = $response->get_headers();
