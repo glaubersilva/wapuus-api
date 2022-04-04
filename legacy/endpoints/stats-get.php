51ba9d61363f99df0c30802a200806f1babd8065
Glauber Silva <info@glaubersilva.me>
2022-04-04 10:34:05 -0300

    Format stats-get file to follow the WPCS

--- legacy/endpoints/stats-get.php
+++ legacy/endpoints/stats-get.php
@@ -1,9 +1,79 @@
 <?php
-
 /**
- * API V1 files - Legacy Code was left in the project just to demonstrate how to extend the WP API without using classes.
+ * The legacy API V1 endpoint for "stats get".
+ *
+ * @package Wapuus_API
+ * @author Glauber Silva <info@glaubersilva.me>
+ * @link https://glaubersilva.me/
  */
 
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Register the "stats get" endpoint.
+ */
+function wapuus_api_register_stats_get() {
+
+	register_rest_route(
+		'wapuus-api/v1',
+		'/stats',
+		array( // The callback to the "resource schema" which is the same for all methods (POST, GET, DELETE etc.) that the endpoint accepts.
+			'schema' => array( \Wapuus_API\Src\Classes\Schemas\Stats_Resource::get_instance(), 'schema' ), // https://developer.wordpress.org/rest-api/extending-the-rest-api/schema/#resource-schema <<< Reference.
+			array(
+				'methods'             => WP_REST_Server::READABLE,
+				'args'                => wapuus_api_stats_get_args(),
+				'permission_callback' => 'wapuus_api_stats_get_permissions_check',
+				'callback'            => 'wapuus_api_stats_get',
+			),
+		)
+	);
+}
+add_action( 'rest_api_init', 'wapuus_api_register_stats_get' );
+
+/**
+ * Schema of the expected arguments for the "stats get" endpoint.
+ *
+ * Reference: https://developer.wordpress.org/rest-api/extending-the-rest-api/schema/#argument-schema
+ *
+ * @return array Arguments.
+ */
+function wapuus_api_stats_get_args() {
+
+	$args = array();
+
+	return $args;
+}
+
+/**
+ * Permission callback for the "stats get" endpoint.
+ *
+ * @param WP_REST_Request $request The current request object.
+ *
+ * @return true|WP_Error Returns true on success or a WP_Error if it does not pass on the permissions check.
+ */
+function wapuus_api_stats_get_permissions_check( $request ) {
+
+	if ( ! is_user_logged_in() ) {
+		/**
+		 * To better understand the "client error responses", check the link below:
+		 * https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses
+		 */
+		$response = new WP_Error( 'Unauthorized', __( 'User does not have permission.', 'wapuus-api' ), array( 'status' => 401 ) );
+		return rest_ensure_response( $response );
+	}
+
+	return true;
+}
+
+/**
+ * Callback for the "stats get" endpoint.
+ *
+ * @param WP_REST_Request $request The current request object.
+ *
+ * @return WP_REST_Response|WP_Error If response generated an error, WP_Error, if response
+ *                                   is already an instance, WP_REST_Response, otherwise
+ *                                   returns a new WP_REST_Response instance.
+ */
 function wapuus_api_stats_get( $request ) {
 
 	$user = wp_get_current_user();
@@ -31,45 +101,4 @@ function wapuus_api_stats_get( $request ) {
 	}
 
 	return rest_ensure_response( $stats );
-}
-
-function wapuus_api_stats_get_permission_callback(){
-
-	if ( ! is_user_logged_in() ) {
-
-		$response = new \WP_Error( 'error', 'User does not have permission.', array( 'status' => 401 ) );
-		return rest_ensure_response( $response );
-
-	}
-
-	return true;
-}
-
-function wapuus_api_register_stats_get() {
-
-	register_rest_route(
-		'wapuus-api/v1',
-		'/stats',
-		array( // Isso declara o Schema do endpoint. Note que o schema é o mesmo para todos os métodos que o endpoint aceita.
-			'schema' => array( \Wapuus_API\Src\Classes\Schemas\Stats_Resource::get_instance(), 'schema' ),
-			array(
-				'methods'  => WP_REST_Server::READABLE, // GET
-				'callback' => 'wapuus_api_stats_get',
-				'permission_callback' => 'wapuus_api_stats_get_permission_callback',
-				'args' => wapuus_api_stats_get_args(),
-			),
-		)
-	);
-
-}
-add_action( 'rest_api_init', 'wapuus_api_register_stats_get' );
-
-function wapuus_api_stats_get_args(){
-	$args = array(
-		/*'none' => array(
-			'description' => 'If logged, return the current user stats.',
-		),*/
-	);
-
-	return $args;
 }
