8c979c58e5f25286e69ce63b1437dd97343f3d77
Glauber Silva <glauber.silva@givewp.com>
2026-02-01 17:32:00 -0300

    refactor: simplify loading for v2 endpoints

--- src/core/endpoints/class-endpoints-service-provider.php
+++ src/core/endpoints/class-endpoints-service-provider.php
@@ -2,6 +2,11 @@
 /**
  * Endpoints Service Provider for the Wapuus API plugin.
  *
+ * Registers and boots API V2 endpoints. Endpoints are registered explicitly
+ * to avoid accidentally loading non-endpoint classes from the same directory.
+ *
+ * @link https://developer.wordpress.org/rest-api/
+ *
  * @package Wapuus_API
  * @author Glauber Silva <info@glaubersilva.me>
  * @link https://glaubersilva.me/
@@ -9,10 +14,8 @@
 
 namespace Wapuus_API\Src\Core\Endpoints;
 
-use Wapuus_API\Src\Core\Load_Endpoints_V2;
+use Wapuus_API\Src\Interfaces\Endpoint;
 use Wapuus_API\Src\Interfaces\Service_Provider_Interface;
-
-use function wapuus_api;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -22,20 +25,85 @@ defined( 'ABSPATH' ) || exit;
 class Endpoints_Service_Provider implements Service_Provider_Interface {
 
 	/**
+	 * Endpoint classes (implement Endpoint interface).
+	 * "Endpoint Wrap" approach: https://carlalexander.ca/designing-system-wordpress-rest-api-endpoints/
+	 *
+	 * @var string[]
+	 */
+	private $endpoint_classes = array(
+		Comments_Delete::class,
+		Comments_Get::class,
+		Comments_Post::class,
+		Images_Delete::class,
+		Images_Get::class,
+		Images_Get_One::class,
+		Images_Post::class,
+		Password_Lost::class,
+		Password_Reset::class,
+		Stats_Get::class,
+		Users_Get::class,
+		Users_Post::class,
+	);
+
+	/**
+	 * Instances of endpoint classes to register.
+	 *
+	 * @var Endpoint[]
+	 */
+	private $endpoints = array();
+
+	/**
 	 * Registers bindings in the container.
 	 *
 	 * @return void
 	 */
 	public function register() {
-		wapuus_api()->singleton( Load_Endpoints_V2::class );
+		// No container bindings for endpoints.
 	}
 
 	/**
-	 * Boots the endpoints (Load_Endpoints_V2 constructor registers routes).
+	 * Boots the endpoints: instantiates endpoints and registers routes on rest_api_init.
 	 *
 	 * @return void
 	 */
 	public function boot() {
-		wapuus_api( Load_Endpoints_V2::class );
+		foreach ( $this->endpoint_classes as $endpoint_class ) {
+			$this->endpoints[] = new $endpoint_class();
+		}
+
+		add_action( 'rest_api_init', array( $this, 'register_endpoints' ) );
+	}
+
+	/**
+	 * Register all endpoints with the WordPress REST API.
+	 *
+	 * @return void
+	 */
+	public function register_endpoints() {
+		foreach ( $this->endpoints as $endpoint ) {
+			$this->register_endpoint( $endpoint );
+		}
+	}
+
+	/**
+	 * Register a single endpoint with the WordPress REST API.
+	 *
+	 * @param Endpoint $endpoint Concrete class implementing the Endpoint interface.
+	 * @return void
+	 */
+	private function register_endpoint( Endpoint $endpoint ) {
+		register_rest_route(
+			'wapuus-api/v2',
+			$endpoint->get_path(),
+			array(
+				'schema' => $endpoint->get_schema(),
+				array(
+					'methods'             => $endpoint->get_methods(),
+					'args'                => $endpoint->get_arguments(),
+					'permission_callback' => $endpoint->get_permission_callback(),
+					'callback'            => $endpoint->get_callback(),
+				),
+			)
+		);
 	}
 }
